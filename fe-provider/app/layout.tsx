0591b6047fbfa1f103496bcc499fb9d9b0426a4e
Aryan Pachori <aryanpachori03@gmail.com>
2024-08-09 05:32:25 +0530

    schema update

--- fe-provider/app/layout.tsx
+++ fe-provider/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Upchain",
+  title: "Upchain(seller)",
   description: "Empower your projects with solana projects",
 };
 
